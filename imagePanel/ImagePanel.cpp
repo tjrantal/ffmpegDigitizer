2d4634607ec8e98b4f42fe540b3ea28eb6bfd280
Timo Rantalainen <tjrantal@gmail.com>
2013-06-21 13:48:06 +0300

    Reorganized files, went back to one header and one cpp file per class

--- imagePanel/ImagePanel.cpp
+++ imagePanel/ImagePanel.cpp
@@ -50,3 +50,19 @@ void ImagePanel::render(wxDC&  dc)
 {
     dc.DrawBitmap( image, 0, 0, false );
 }
+
+/*EVENT TABLE*/
+BEGIN_EVENT_TABLE(ImagePanel, wxPanel)
+	// some useful events
+	/*
+	 EVT_MOTION(wxImagePanel::mouseMoved)
+	 EVT_LEFT_DOWN(wxImagePanel::mouseDown)
+	 EVT_LEFT_UP(wxImagePanel::mouseReleased)
+	 EVT_RIGHT_DOWN(wxImagePanel::rightClick)
+	 EVT_LEAVE_WINDOW(wxImagePanel::mouseLeftWindow)
+	 EVT_KEY_DOWN(wxImagePanel::keyPressed)
+	 EVT_KEY_UP(wxImagePanel::keyReleased)
+	 EVT_MOUSEWHEEL(wxImagePanel::mouseWheelMoved)
+	 */
+	EVT_PAINT(ImagePanel::paintEvent) // catch paint events
+END_EVENT_TABLE()
--- imagePanel/ImagePanel.h
+++ imagePanel/ImagePanel.h
@@ -28,18 +28,5 @@
 		    DECLARE_EVENT_TABLE()
 		    
 	};
-	BEGIN_EVENT_TABLE(ImagePanel, wxPanel)
-		// some useful events
-		/*
-		 EVT_MOTION(wxImagePanel::mouseMoved)
-		 EVT_LEFT_DOWN(wxImagePanel::mouseDown)
-		 EVT_LEFT_UP(wxImagePanel::mouseReleased)
-		 EVT_RIGHT_DOWN(wxImagePanel::rightClick)
-		 EVT_LEAVE_WINDOW(wxImagePanel::mouseLeftWindow)
-		 EVT_KEY_DOWN(wxImagePanel::keyPressed)
-		 EVT_KEY_UP(wxImagePanel::keyReleased)
-		 EVT_MOUSEWHEEL(wxImagePanel::mouseWheelMoved)
-		 */
-		EVT_PAINT(ImagePanel::paintEvent) // catch paint events
-	END_EVENT_TABLE()
+
 #endif
