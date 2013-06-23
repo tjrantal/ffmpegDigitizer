dc2cf47cb6d8e36df9ca7351fe5774ee9957b002
Timo Rantalainen <tjrantal@gmail.com>
2013-06-23 16:08:53 +0300

    Display video on screen works, memcpy hack does not

---
+++ digitizerFrame/DigitizerFrame.cpp
@@ -0,0 +1,118 @@
+/*
+Copyright (C) 2013  Timo Rantalainen (tjrantal at gmail dot com)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+For a copy of the GNU General Public License, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef DIGITIZERFRAME_H
+	#include "DigitizerFrame.h"
+#endif
+
+DigitizerFrame::DigitizerFrame(const wxString& title, const wxPoint& pos, const wxSize& size)
+       : wxFrame(NULL, -1, title, pos, size)
+{
+	wxMenu *menuFile = new wxMenu;
+
+    menuFile->Append( ID_About, _("&About...") );
+    menuFile->AppendSeparator();
+    menuFile->Append( ID_Quit, _("E&xit") );
+
+    wxMenuBar *menuBar = new wxMenuBar;
+    menuBar->Append( menuFile, _("&File") );
+
+    SetMenuBar( menuBar );
+
+    CreateStatusBar();
+    SetStatusText( _("Welcome to Digitizer!") );
+	/*Add text field for results*/
+	resultsText = new wxTextCtrl(this,-1,_("Button hasn't been pressed"),wxPoint(10,50),wxSize(200,40),wxTE_MULTILINE);
+	/*Button for reading markers in*/
+	openMarkerFile 	= new wxButton(this,ID_picker,_("Open marker file"),wxPoint(10,150));
+	Connect(ID_picker,wxEVT_COMMAND_BUTTON_CLICKED,wxCommandEventHandler(DigitizerFrame::OpenFile),NULL,this);
+	/*Slider for browsing video*/
+	slider = new wxSlider(this,ID_slider,0,0,100,wxPoint(300,470),wxSize(400,40));
+	//Connect(ID_slider,EVT_SCROLL,wxCommandEventHandler(DigitizerFrame::ScrollVideo),NULL,this);
+	/*Connect button click events*/
+	resultsText->Connect(wxEVT_LEFT_DOWN,wxMouseEventHandler(DigitizerFrame::LeftButtonDown), NULL,this);
+	resultsText->Connect(wxEVT_LEFT_UP,wxMouseEventHandler(DigitizerFrame::LeftButtonUp),NULL,this);
+	/*Connect events to the main frame*/
+	Connect(wxEVT_LEFT_DOWN,wxMouseEventHandler(DigitizerFrame::LeftButtonDown), NULL,this);
+	Connect(wxEVT_LEFT_UP,wxMouseEventHandler(DigitizerFrame::LeftButtonUp),NULL,this);
+	
+	imagePanel = new ImagePanel(this,ID_panel,_("DSC_0001.JPG"),wxBITMAP_TYPE_JPEG,wxPoint(200,10),wxSize(750,380));
+	videoReader = new VideoReader("GOPR0085.MP4",10);
+	videoReader->readFrames(); 
+	imagePanel->setImage(videoReader->leveys,videoReader->korkeus,videoReader->video[4],true);
+}
+
+/*Button event handling*/
+void DigitizerFrame::LeftButtonDown(wxMouseEvent& WXUNUSED(event)){
+	//does nothing
+}
+
+void DigitizerFrame::LeftButtonUp(wxMouseEvent& WXUNUSED(event)){
+	//Does nothing
+}
+
+void DigitizerFrame::OnAbout(wxCommandEvent& WXUNUSED(event))
+{
+	SetStatusText( _("Tried_about") );
+}
+
+void DigitizerFrame::OnQuit(wxCommandEvent& WXUNUSED(event))
+{
+    Close(true);
+}
+
+void DigitizerFrame::OpenFile(wxCommandEvent& event){
+	
+	/*Open marker file*/
+		wxFileDialog openFileDialog(this, _("Open TAB file"), _(""), _(""),
+	_("TAB files (*.tab)|*.tab"), wxFD_OPEN|wxFD_FILE_MUST_EXIST);
+	if (openFileDialog.ShowModal() == wxID_CANCEL){
+		SetStatusText(_("No marker file opened"));
+		resultsText->ChangeValue(_("No marker file opened"));
+	}else{
+		// save the current contents in the file;
+		// this can be done with e.g. wxWidgets output streams:
+		openFile = new wxFile( openFileDialog.GetPath(), wxFile::write_append );
+		if (!openFile->IsOpened())
+		{
+			SetStatusText(_("Could not open save file!"));
+			resultsText->ChangeValue(_("Could not open save file!"));
+			openFile = NULL;
+		}else{
+			//READ MARKERS HERE
+			/*Close the save file*/
+			if (openFile != NULL){
+				openFile->Close();
+			}
+			SetStatusText( _("Markers read"));
+		}
+	}
+	
+}
+
+void DigitizerFrame::ScrollVideo(wxScrollEvent &event){
+	int currentVal = slider->GetValue();
+	resultsText->ChangeValue(wxString::Format(wxT("%s %d"),"Frame #", currentVal));
+	SetStatusText(wxString::Format(wxT("%s %d"),"Frame #", currentVal));
+}
+
+/*Event table*/
+BEGIN_EVENT_TABLE(DigitizerFrame, wxFrame)
+    EVT_MENU(ID_Quit,  		DigitizerFrame::OnQuit)
+    EVT_MENU(ID_About, 		DigitizerFrame::OnAbout)
+	EVT_SCROLL(DigitizerFrame::ScrollVideo)	
+END_EVENT_TABLE()
+
---
+++ digitizerFrame/DigitizerFrame.h
@@ -0,0 +1,76 @@
+/*
+Copyright (C) 2013  Timo Rantalainen (tjrantal at gmail dot com)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+For a copy of the GNU General Public License, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef DIGITIZERFRAME_H
+#define DIGITIZERFRAME_H
+
+#include <wx/wx.h>
+#include <wx/event.h>		//Listening to mouse events
+#include <wx/textctrl.h>	//Text field for displaying results
+#include <wx/button.h>		//For resetting the clock
+#include <wx/filedlg.h>		//For selecting a file to read
+#include <wx/file.h>		//For reading a file
+#include <wx/slider.h>		//For slider
+#ifndef IMAGEPANEL_H
+	#include "../imagePanel/ImagePanel.h"
+#endif
+#ifndef VIDEOREADER_H
+	#include "../videoReader/VideoReader.h"
+#endif
+
+class DigitizerFrame: public wxFrame
+{
+public:
+    DigitizerFrame(const wxString& title, const wxPoint& pos, const wxSize& size);
+private:
+	//Variables
+	wxTextCtrl		*resultsText;			//For displaying text in screen
+	clock_t			*clockCounts,initClock;	//For storing processor times
+	wxButton		*resetButton;			//For resetting the clock
+	float			elapsedTime;				//time since first click
+	bool			timeRunning;				//to indicate whether the first click has passed
+	FILE			*resultFile;			//File to save to
+	wxButton 		*openMarkerFile;		//Pick a file to save to
+	wxFile 			*openFile;
+	wxSlider		*slider;				//Slider
+	ImagePanel		*imagePanel;			//ImagePanel
+	VideoReader		*videoReader;				//videoReader
+    void OnQuit(wxCommandEvent &event);
+    void OnAbout(wxCommandEvent &event);
+	void LeftButtonDown(wxMouseEvent &event);
+	void LeftButtonUp(wxMouseEvent &event);
+	void ResetClock(wxCommandEvent &event);
+	void OpenFile(wxCommandEvent &event);
+	void ScrollVideo(wxScrollEvent &event);
+    DECLARE_EVENT_TABLE()
+	//wxDECLARE_EVENT_TABLE();
+};
+
+enum
+{
+    ID_Quit = 1,
+    ID_About,
+	ID_Reset,
+	ID_picker,
+	ID_slider,
+	ID_panel,
+};
+
+
+
+
+
+#endif
