5c2c035eb0f59693699dabba33f075aeb846b070
Timo Rantalainen <tjrantal@gmail.com>
2013-07-01 22:15:46 +0300

    Change to working with timeStamps?

--- videoReader/VideoReader.h
+++ videoReader/VideoReader.h
@@ -39,6 +39,7 @@ For a copy of the GNU General Public License, see <http://www.gnu.org/licenses/>
 		AVPacket packet;	/**< Packet*/
 		int lastKeyframePacket;	/**< Latest keyframe, packets from this to the frame of interest need to be decoded to get a full frame*/
 		int 	decode_picture_number;	/**< Number of picture in series*/
+		/*ADD tstamp -> use timestamp for searching in addition to packete number*/
 	} framePacket;
 	
 	
