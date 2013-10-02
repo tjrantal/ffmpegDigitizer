448b3b4077c35cb091093f188fedd4c27495a890
Timo Rantalainen <tjrantal@gmail.com>
2013-10-02 14:14:23 +1000

    Changed plans -> use sliders for search radius and marker radius

--- digitizerFrame/DigitizerFrame.h
+++ digitizerFrame/DigitizerFrame.h
@@ -56,10 +56,12 @@ private:
 	wxButton 		*openVideoFile;		//Pick a file to save to
 	wxFile 			*openVideo;
 	wxSlider		*slider;				//Slider
+	wxSlider		*searchRadius;				//Slider
+	wxSlider		*markerRadius;				//Slider
 	ImagePanel		*imagePanel;			//ImagePanel
 	VideoReader		*videoReader;			//videoReader
 	MarkerSelector	*markerSelector;		//Marker selector
-	TrackingParameters	*trackingParameters;	//Marker tracking parameters
+	
     void OnQuit(wxCommandEvent &event);
 	void OnQuit(wxCloseEvent& event);
     void OnAbout(wxCommandEvent &event);
